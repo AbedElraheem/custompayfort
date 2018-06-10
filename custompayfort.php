da3bdd1a697891e7abf93758d39cee4bacc83aaf
AbedElraheem <abd_m1986@yahoo.com>
2018-06-11 01:07:45 +0300

    modified:   custompayfort.php 	new file:   updater.php

--- custompayfort.php
+++ custompayfort.php
@@ -21,7 +21,7 @@ Plugin Name: Payfort Plugin
 Plugin URI: php-jo.com
 Description: Add custom payfort for najah.
 Author: AbedElraheem
-Version:  0.0.1
+Version:  0.0.2
 Author URI: php-jo.com
 */
 
@@ -29,6 +29,25 @@ Author URI: php-jo.com
 
 
 include_once 'style.php';
+include_once('updater.php');
+
+if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+    $config = array(
+        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+        'proper_folder_name' => 'Payfort Plugin', // this is the name of the folder your plugin lives in
+        'api_url' => 'https://api.github.com/repos/AbedElraheem/custompayfort', // the GitHub API url of your GitHub repo
+        'raw_url' => 'https://raw.github.com/username/custompayfort/master', // the GitHub raw url of your GitHub repo
+        'github_url' => 'https://github.com/username/custompayfort', // the GitHub url of your GitHub repo
+        'zip_url' => 'https://github.com/username/custompayfort/zipball/master', // the zip url of the GitHub repo
+        'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'requires' => '4.0', // which version of WordPress does your plugin require?
+        'tested' => '4.3', // which version of WordPress is your plugin tested up to?
+        'readme' => 'README.md', // which file to use as the readme for the version number
+        'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+    );
+    new WP_GitHub_Updater($config);
+}
+
 
 add_action('admin_menu', function() {
     add_options_page( 'Payfort plugin settings', 'payfort plugin', 'manage_options', 'payfort-plugin', 'payfort_plugin_page' );
