ef19256cb9cee6a9b032e428f68092a42529f8f9
AbedElraheem <abd_m1986@yahoo.com>
2018-06-11 02:16:28 +0300

    v0.0.6

--- custompayfort.php
+++ custompayfort.php
@@ -4,7 +4,7 @@ Plugin Name: Payfort Plugin
 Plugin URI: php-jo.com
 Description: Add custom payfort for najah.
 Author: AbedElraheem
-Version:  0.0.5
+Version:  0.0.6
 Author URI: php-jo.com
 GitHub Plugin URI: AbedElraheem/custompayfort
 */
